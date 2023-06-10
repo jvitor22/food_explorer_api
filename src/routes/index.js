8477c7ebacd6887071a62ffdbeb3fa2b31b8844c
jvitor22 <j.vitor.a@gmail.com>
2023-06-09 23:45:38 -0300

    creat sessionsController and sessions.routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express")
 const usersRouter = require("./users.routes")
 const platesRouter = require("./plates.routes")
 const ingredientsRouter = require("./ingredients.routes")
+const sessionsRouter = require("./sessions.routes")
 
 const routes = Router()
 routes.use("/users", usersRouter)
 routes.use("/plates", platesRouter)
 routes.use("/ingredients", ingredientsRouter)
+routes.use("/sessions", sessionsRouter)
 
 
 module.exports = routes
